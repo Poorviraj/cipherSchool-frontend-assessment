87d4116fb0085a2f9c9ac32ccbe03b0db082c507
Poorviraj Singh Nirwan <112056229+Poorviraj@users.noreply.github.com>
2025-10-29 02:32:11 +0530

    Update API URL to production endpoint

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000/api/auth";
+const API_URL = "https://cipherschool-assessment-backend.onrender.com/api/auth";
 
 
 export const registerApi = async (userData) => {
